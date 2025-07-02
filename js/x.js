2d568c10a3fa7251b8979727c59e31e430c65f3a
8jd <84992744+8jd@users.noreply.github.com>
2025-07-02 11:58:27 -0400

    Update x.js

--- js/x.js
+++ js/x.js
@@ -1,6 +1,6 @@
 const videos = [
     "https://files.catbox.moe/vy665x.mp4",
-    "https://files.catbox.moe/bg7hdv.mp4"
+    "https://files.catbox.moe/bg7hdv.mp4",
     "https://files.catbox.moe/l9yi9r.mp4",
     "https://files.catbox.moe/elg9zl.mp4",
     "https://files.catbox.moe/es017e.mp4"
