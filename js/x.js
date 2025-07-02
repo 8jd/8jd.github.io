3d2056041b4494a81d880639f2eb5e31d6c4f34f
8jd <84992744+8jd@users.noreply.github.com>
2025-07-02 11:55:53 -0400

    Update x.js

--- js/x.js
+++ js/x.js
@@ -1,9 +1,9 @@
 const videos = [
-    "video/a.mp4",
-    "video/b.mp4",
-    "video/c.mp4",
-    "video/d.mp4",
-    "video/e.mp4"
+    "https://files.catbox.moe/vy665x.mp4",
+    "https://files.catbox.moe/bg7hdv.mp4"
+    "https://files.catbox.moe/l9yi9r.mp4",
+    "https://files.catbox.moe/elg9zl.mp4",
+    "https://files.catbox.moe/es017e.mp4"
 ];
 const randInd = Math.floor(Math.random() * videos.length);
 const x = videos[randInd];
