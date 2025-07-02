c3af7a57f60d2b91446e55fc86032d00880202a3
8jd <84992744+8jd@users.noreply.github.com>
2025-07-02 11:41:16 -0400

    a

--- js/x.js
+++ js/x.js
@@ -3,7 +3,7 @@ const videos = [
     "video/x.mp4",
     "video/i.mp4",
     "video/l.mp4",
-    "video/е.mp4"
+    "video/еxile.mp4"
 ];
 const randInd = Math.floor(Math.random() * videos.length);
 const x = videos[randInd];
